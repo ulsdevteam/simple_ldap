cfea97704b0167d7a23655875b3f08529f74ea0f
Ben Chavet <ben.chavet@lullabot.com>
2012-11-21 13:23:35 -0600

    Document hook_sync_user_to_ldap().

--- simple_ldap_user/simple_ldap_user.api.php
+++ simple_ldap_user/simple_ldap_user.api.php
@@ -34,3 +34,20 @@
  * reset()
  * hash($key, $value)
  */
+
+/**
+ * Synchronizes a Drupal user to LDAP.
+ *
+ * This hook is called when simple_ldap_user needs to synchronize Drupal user
+ * data to LDAP.
+ *
+ * This example sets the LDAP employeeType attribute to "full-time"
+ *
+ * @param StdClass $user
+ *   The full Drupal user object that is being synchronized.
+ */
+function hook_sync_user_to_ldap($user) {
+  $ldap_user = SimpleLdapUser::singleton($user->name);
+  $ldap_user->employeeType = 'full-time';
+  $ldap_user->save();
+}
